ee7c63ebae01e0b674e3f981aea12d9aa8a707f1
Steviebeenz <bowyers.nz@gmail.com>
2019-09-17 16:58:50 +1200

    Feats

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,61 +1,62 @@
 <template>
 <div id="B1xaXeyCUH">
+  <mdc-fab class="rkg8_-1CIB" icon="add"></mdc-fab>
+<mdc-fab class="BJqnMk0LB" icon="search"></mdc-fab>
+  <mdc-fab class="ByzAMyAIS" icon="settings"></mdc-fab>
   <mdc-drawer class="rJVSFlyA8r" open toolbar-spacer persistent toggle-on="toggledrawer">
     <mdc-drawer-list class="SkrHKgkRIH">
       <mdc-drawer-item class="Sy8Hte1AUB" start-icon="inbox">Inbox</mdc-drawer-item>
-    
+
       <mdc-drawer-item class="ByvrKly0Ur" start-icon="send">Sent Mail</mdc-drawer-item>
-    
+
       <mdc-drawer-item class="rkdSKl1ALB" start-icon="drafts">Drafts</mdc-drawer-item>
     </mdc-drawer-list>
   </mdc-drawer>
   <mdc-card class="HkHB-ZkRIS">
     <mdc-card-media class="HJ8HWWJRUr" height="160" src="https://material-components-web.appspot.com/images/16-9.jpg"></mdc-card-media>
-  
-    <mdc-card-header class="HJDH-Zk0UH" title="Title" subtitle="Subtitle"></mdc-card-header>
-  
+
+    <mdc-card-header class="HJDH-Zk0UH" title="Let's Get This Going..." subtitle="Subtitle"></mdc-card-header>
+
     <mdc-card-actions class="HydHWWyRUr">
       <mdc-card-action-button class="HktB--kAIH">Action 1</mdc-card-action-button>
-    
+
       <mdc-card-action-button class="HkcBbWkALH">Action 2</mdc-card-action-button>
     </mdc-card-actions>
   </mdc-card>
   <mdc-card class="HJxE-J0UB">
     <mdc-card-media class="HylgN-1CUr" height="160" src="https://material-components-web.appspot.com/images/16-9.jpg"></mdc-card-media>
-  
+
     <mdc-card-header class="B1-eEbJ0Lr" title="Title" subtitle="Subtitle"></mdc-card-header>
-  
+
     <mdc-card-actions class="BkMlEbyC8S">
       <mdc-card-action-button class="S1XeVb108H">Action 1</mdc-card-action-button>
-    
+
       <mdc-card-action-button class="BJNg4WJRIH">Action 2</mdc-card-action-button>
     </mdc-card-actions>
   </mdc-card>
-  <mdc-fab class="rkg8_-1CIB" icon="add"></mdc-fab>
   <mdc-card class="By78MkA8S">
     <mdc-card-media class="ryeQLMJRUr" height="160" src="https://material-components-web.appspot.com/images/16-9.jpg"></mdc-card-media>
-  
+
     <mdc-card-header class="HyWQUG1RIH" title="Title" subtitle="Subtitle"></mdc-card-header>
-  
+
     <mdc-card-actions class="S1GmIGy0Lr">
       <mdc-card-action-button class="rJQXLMJR8B">Action 1</mdc-card-action-button>
-    
+
       <mdc-card-action-button class="SkVQUMkAUB">Action 2</mdc-card-action-button>
     </mdc-card-actions>
   </mdc-card>
   <mdc-card class="B1PLGkAUB">
     <mdc-card-media class="S1xPUfyALB" height="160" src="https://material-components-web.appspot.com/images/16-9.jpg"></mdc-card-media>
-  
+
     <mdc-card-header class="ryWwUfJRUB" title="Title" subtitle="Subtitle"></mdc-card-header>
-  
+
     <mdc-card-actions class="rJzPIMkALS">
       <mdc-card-action-button class="SkQD8zyCUB">Action 1</mdc-card-action-button>
-    
+
       <mdc-card-action-button class="B1Ew8zkA8B">Action 2</mdc-card-action-button>
     </mdc-card-actions>
   </mdc-card>
-  <mdc-fab class="BJqnMk0LB" icon="search"></mdc-fab>
-  <mdc-fab class="ByzAMyAIS" icon="settings"></mdc-fab>
+
 </div>
 </template>
 
