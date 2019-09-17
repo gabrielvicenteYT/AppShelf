b93a7c5fb0b1c3daf7af0503f505d426345f35a5
Steviebeenz <bowyers.nz@gmail.com>
2019-09-17 16:42:07 +1200

    Initial Commit

---
+++ src/router/index.js
@@ -0,0 +1,14 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+
+import Home from '@/pages/Home'
+import Search from '@/pages/Search'
+
+Vue.use(Router)
+
+export default new Router({
+  routes: [
+    { path: '/', name: 'Home', component: Home },
+    { path: '/search', name: 'Search', component: Search }
+  ]
+})
